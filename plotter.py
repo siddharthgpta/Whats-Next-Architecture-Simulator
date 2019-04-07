31f3ba14e841f7674949ee4b7b2bca145b1875bb
Sid <gupt232@usc.edu>
2019-04-06 17:28:06 -0700

    Added 3rd diagram for comparision

--- plotter.py
+++ plotter.py
@@ -2,17 +2,22 @@ import matplotlib.pyplot as plt
 
 l1 = []
 l2 = []
+l3 = []
 
-with open('output1.txt', 'r') as f:
+with open('output_GM_SRAM.txt', 'r') as f:
     l1 = [int(x) for x in f.readline().split(', ')]
 
-with open('output2.txt', 'r') as f:
+with open('output_AM_SRAM.txt', 'r') as f:
     l2 = [int(x) for x in f.readline().split(', ')]
+
+with open('output_AM_ReRAM.txt', 'r') as f:
+    l3 = [int(x) for x in f.readline().split(', ')]
 
 x = 1
 size = int(x * len(l1))
 l1 = l1[:size]
 l2 = l2[:size]
+l3 = l3[:size]
 
 error = [0] * len(l1)
 i = 0
@@ -20,8 +25,15 @@ for x,y in zip(l1, l2):
     error[i] = (x-y)/x
     i += 1
 
-plt.title("SRAM multiplication results. Avg. error: {0}".format(sum(error)/i))
+error1 = [0] * len(l1)
+j = 0
+for x,y in zip(l1, l3):
+    error1[j] = (x-y)/x
+    j += 1
+
+plt.title("SRAM multiplication results. Avg. error: {0}(SRAM), {1}(ReRAM)".format(sum(error)/i, sum(error1)/j))
 line1, = plt.plot(l1)
 line2, = plt.plot(l2)
-plt.legend((line1, line2), ('General multiplier', 'Approximate multiplier'))
+line3, = plt.plot(l3)
+plt.legend((line1, line2, line3), ('General multiplier', 'Approximate multiplier (SRAM)', 'Approximate multiplier (ReRAM)'))
 plt.show()
